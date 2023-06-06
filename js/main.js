cabebaf56568c82844c61c1bdebf373e70fe0f82
Fr1son <ihor.malenkov18@gmail.com>
2023-06-06 10:41:38 +0300

    update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function(){
   $('.slider-blog__inner').slick({
     dots:true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrov: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
   });
 
 });
